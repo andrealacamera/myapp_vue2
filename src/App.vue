22e0e600745fdc46110c36d57bdb615d2e22a87d
andrealacamera <andrea.lacamera0@gmail.com>
2020-07-28 15:32:16 +0200

    First version

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <ul class="nav justify-content-center">
+        <li class="nav-item"><router-link to="/">Home</router-link></li>
+        <li class="nav-item"><router-link to="/about">About</router-link></li>
+        <li class="nav-item"><router-link to="/contacts">Contacts</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+</style>
